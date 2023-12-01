4d649825801948db0fc5b4dde18062fa38acb635
ZLAllen <zhaoqi_li@yahoo.com>
2023-11-30 20:31:10 -0500

    Make copy for p2 start project

---
+++ P2_start_solution/ShoppingCartServiceTests/BusinessLogic/ShippingCalculatorTest.cs
@@ -0,0 +1,126 @@
+﻿using FluentAssertions;
+using ShoppingCartService.BusinessLogic;
+using ShoppingCartService.DataAccess.Entities;
+using ShoppingCartService.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using CartTuple = System.Tuple<ShoppingCartServiceTests.BusinessLogic.ShippingCalculatorTest.AddressType, ShoppingCartService.Models.CustomerType, ShoppingCartService.Models.ShippingMethod>;
+
+namespace ShoppingCartServiceTests.BusinessLogic
+{
+
+    public class ShippingCalculatorTest
+    {
+
+        public enum AddressType
+        {
+            SameCity,
+            SameCountry,
+            International
+        }
+
+        private static readonly Address SameCityAddress = new()
+        {
+            Country = "USA",
+            City = "New York City"
+        };
+
+        private static readonly Address SameCountryAddress = new()
+        {
+            Country = "USA",
+            City = "Chicago"
+        };
+
+        private static readonly Address InternationAddress = new()
+        {
+            Country = "Germany"
+        };
+
+        public static readonly Dictionary<AddressType, Address> Addresses = new()
+        {
+            {AddressType.SameCity,  SameCityAddress},
+            {AddressType.SameCountry, SameCountryAddress},
+            {AddressType.International, InternationAddress}
+        };
+
+        private readonly Dictionary<Tuple<AddressType, CustomerType, ShippingMethod>, double> ShippingCosts = new()
+        {
+            {new CartTuple(AddressType.SameCity, CustomerType.Standard, ShippingMethod.Standard),  6.0d },
+            {new CartTuple(AddressType.SameCity, CustomerType.Standard, ShippingMethod.Expedited),  7.2d },
+            {new CartTuple(AddressType.SameCity, CustomerType.Standard, ShippingMethod.Priority),  12.0d },
+            {new CartTuple(AddressType.SameCity, CustomerType.Standard, ShippingMethod.Express),  15.0d },
+            {new CartTuple(AddressType.SameCity, CustomerType.Premium, ShippingMethod.Standard),  6.0d },
+            {new CartTuple(AddressType.SameCity, CustomerType.Premium, ShippingMethod.Expedited),  6.0d },
+            {new CartTuple(AddressType.SameCity, CustomerType.Premium, ShippingMethod.Priority),  6.0d },
+            {new CartTuple(AddressType.SameCity, CustomerType.Premium, ShippingMethod.Express),  15.0d },
+            {new CartTuple(AddressType.SameCountry, CustomerType.Standard, ShippingMethod.Standard),  12.0d },
+            {new CartTuple(AddressType.SameCountry, CustomerType.Standard, ShippingMethod.Expedited),  14.4d },
+            {new CartTuple(AddressType.SameCountry, CustomerType.Standard, ShippingMethod.Priority),  24.0d },
+            {new CartTuple(AddressType.SameCountry, CustomerType.Standard, ShippingMethod.Express),  30.0d },
+            {new CartTuple(AddressType.SameCountry, CustomerType.Premium, ShippingMethod.Standard),  12.0d },
+            {new CartTuple(AddressType.SameCountry, CustomerType.Premium, ShippingMethod.Expedited),  12.0d },
+            {new CartTuple(AddressType.SameCountry, CustomerType.Premium, ShippingMethod.Priority),  12.0d },
+            {new CartTuple(AddressType.SameCountry, CustomerType.Premium, ShippingMethod.Express),  30.0d },
+            {new CartTuple(AddressType.International, CustomerType.Standard, ShippingMethod.Standard),  90.0d },
+            {new CartTuple(AddressType.International, CustomerType.Standard, ShippingMethod.Expedited),  108.0d },
+            {new CartTuple(AddressType.International, CustomerType.Standard, ShippingMethod.Priority),  180.0d },
+            {new CartTuple(AddressType.International, CustomerType.Standard, ShippingMethod.Express),  225.0d },
+            {new CartTuple(AddressType.International, CustomerType.Premium, ShippingMethod.Standard),  90.0d },
+            {new CartTuple(AddressType.International, CustomerType.Premium, ShippingMethod.Expedited),  90.0d },
+            {new CartTuple(AddressType.International, CustomerType.Premium, ShippingMethod.Priority),  90.0d },
+            {new CartTuple(AddressType.International, CustomerType.Premium, ShippingMethod.Express),  225.0d },
+        };
+
+        public class CartTestData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                var allAddressTypes = Enum.GetValues(typeof(AddressType)).Cast<AddressType>();
+                var allCustomerTypes = Enum.GetValues(typeof(CustomerType)).Cast<CustomerType>();
+                var allShippingMethods = Enum.GetValues(typeof(ShippingMethod)).Cast<ShippingMethod>();
+                var objs = from addressType in allAddressTypes
+                            from customerType in allCustomerTypes
+                            from shippingMethod in allShippingMethods
+                            select new object[] { new Cart { ShippingAddress = Addresses[addressType], CustomerType = customerType, ShippingMethod = shippingMethod, Items = CreateCartItems()},
+                            new CartTuple(addressType, customerType, shippingMethod)};
+
+                foreach (var obj in objs)
+                {
+                    yield return obj;
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+            public List<Item> CreateCartItems() => new()
+            {
+                new() {Quantity = 1},
+                new() {Quantity = 2},
+                new() {Quantity = 3},
+            };
+        }
+
+        public ShippingCalculator CreateShippingCalculator(Address address)
+        {
+            return new ShippingCalculator(address);
+        }
+
+        [Theory]
+        [ClassData(typeof(CartTestData))]
+        public void Can_calculate_shipping_cost(Cart cart, CartTuple cartTuple)
+        {
+            ShippingCalculator sut = CreateShippingCalculator(SameCityAddress);
+
+            double result = sut.CalculateShippingCost(cart);
+
+            result.Should().BeApproximately(ShippingCosts[cartTuple], 0.01d);
+        }
+
+    }
+}
---
+++ P2_start_solution/ShoppingCartServiceTests/BusinessLogic/Validation/AddressValidatorTest.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using ShoppingCartService.BusinessLogic.Validation;
+using ShoppingCartService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartServiceTests.BusinessLogic.Validation
+{
+    public class TestDataGenerator
+    {
+        public static List<object?[]> GetInvalidAddressTestData() => new()
+        {
+            new object?[] {null},
+            new object?[] {new Address {Country = string.Empty, City = "abc", Street = "abc"}},
+            new object?[] {new Address {Country = "abc", City = string.Empty, Street = "abc"}},
+            new object?[] {new Address {Country = "abc", City = "abc", Street = string.Empty}},
+            new object?[] {new Address {Country = null, City = "abc", Street = "abc"}},
+            new object?[] {new Address {Country = "abc", City = null, Street = "abc"}},
+            new object?[] {new Address {Country = "abc", City = "abc", Street = null}},
+        };
+    }
+
+    public class AddressValidatorTest
+    {
+        public AddressValidator CreateAddressValidator() => new AddressValidator();
+
+
+        [Theory]
+        [MemberData(nameof(TestDataGenerator.GetInvalidAddressTestData), MemberType = typeof(TestDataGenerator))]
+        public void Can_detect_an_invalid_address(Address addr)
+        {
+            AddressValidator sut = CreateAddressValidator();
+
+            bool result = sut.IsValid(addr);
+
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Can_confirm_a_valid_address()
+        {
+            AddressValidator sut = CreateAddressValidator();
+            Address addr = new() { Country = "abc", City = "abc", Street = "abc" };
+
+            bool result = sut.IsValid(addr);
+
+            result.Should().BeTrue();
+        }
+    }
+}
